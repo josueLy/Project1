a8574cf54d3698e33e5f9a6f15cff15e8c1a0fd6
Joshua Ly <josue.joshua.ly@gmail.com>
2022-07-04 11:24:59 -0500

    implementing create method of Business Account

--- prjDemoBC/src/main/java/project1/service/impl/BusinessAccountServiceImpl.java
+++ prjDemoBC/src/main/java/project1/service/impl/BusinessAccountServiceImpl.java
@@ -1,14 +1,31 @@
 package project1.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project1.dto.businessAccount.BusinessAccountDto;
+import project1.model.Bank_Account;
+import project1.model.Business;
 import project1.model.Business_Account;
+import project1.repository.IBankAccountRepository;
+import project1.repository.IBusinessAccountRepository;
+import project1.repository.IBusinessRepository;
 import project1.service.interfaces.IBusinessAccountService;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class BusinessAccountServiceImpl implements IBusinessAccountService {
+
+    @Autowired
+    private IBusinessAccountRepository businessAccountRepository;
+
+    @Autowired
+    private IBusinessRepository businessRepository;
+
+    @Autowired
+    private IBankAccountRepository bankAccountRepository;
+
     @Override
     public Flux<Business_Account> findAll() {
         return null;
@@ -16,7 +33,35 @@ public class BusinessAccountServiceImpl implements IBusinessAccountService {
 
     @Override
     public Mono<Business_Account> save(BusinessAccountDto bussinessDto) {
-        return null;
+
+        Mono<Business> businessMono= businessRepository.findById(bussinessDto.getBusinessId());
+
+        Mono<Bank_Account> bankAccountMono= bankAccountRepository.findById(bussinessDto.getBank_account_id());
+
+        Mono<Business_Account> businessAccountMono= Mono.just(new Business_Account());
+
+        Business_Account businessAccount = new Business_Account();
+
+        businessAccountMono=businessMono.map(result->{
+
+
+           businessAccount.setBusiness(result);
+
+           return businessAccount;
+        });
+
+        businessAccountMono=bankAccountMono.map(result->{
+
+            businessAccount.setAccount(result);
+
+            return  businessAccount;
+        }).flatMap(result->{
+
+            return  businessAccountRepository.save(businessAccount);
+        });
+
+        return  businessAccountMono;
+
     }
 
     @Override
