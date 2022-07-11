905bf5bbaa476d1ac092e7f4f4d216f902652d7f
Rolando <rsanchba@emeal.nttdata.com>
2022-07-11 01:03:15 -0500

    client-transaction-product

---
+++ client-service/src/main/java/com/service/clientservice/model/Business_Account.java
@@ -0,0 +1,15 @@
+package com.service.clientservice.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "business_account")
+@Data
+public class Business_Account {
+
+    @Id
+    private String idBusinessAccount;
+    private Business business;
+    private Bank_Account account;
+}
